77e2232d2d3b01dd860c8f472cea38be785de171
Jaime Zepeda Pina <zepedapina@wisc.edu>
2024-08-06 14:41:28 -0500

    backend progress

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,96 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { MovieCarrousel } from "../../components/movie";
 
-const movies = [
-  {
-    title: "Toy Story",
-    release_date: "1998",
-    certification: "G",
-    runtime: "2h 13m",
-    poster: "movietest.jpg",
-    genres: ["comedy", "children"],
-    rating: 64,
-    tmdbId: "862",
-  },
-  {
-    title: "Inception",
-    release_date: "2012",
-    certification: "TV-MA",
-    runtime: "2h 13m",
-    poster: "inceptiontest.jpg",
-    genres: ["sci-fi", "thriller", "sci-fi", "thriller", "sci-fi", "thriller"],
-    rating: 23,
-    tmdbId: "27205",
-  },
-  {
-    title: "The Lion King",
-    release_date: "1995",
-    certification: "PG",
-    runtime: "2h 13m",
-    poster: "lionkingtest.jpg",
-    genres: ["animation", "adventure"],
-    rating: 70,
-    tmdbId: "8587",
-  },
-  {
-    title: "The Shawshank Redemption",
-    release_date: "2014",
-    certification: "R",
-    runtime: "2h 13m",
-    poster: "interstellartest.jpg",
-    genres: ["action", "sci-fi"],
-    rating: 86,
-    tmdbId: "157336",
-  },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    release_date: "2014",
-    certification: "PG-13",
-    runtime: "2h 13m",
-    poster: "interstellartest.jpg",
-    genres: ["action", "sci-fi"],
-    rating: 86,
-    tmdbId: "157336",
-  },
-  {
-    title: "Interstellar",
-    release_date: "2014",
-    certification: "PG-13",
-    runtime: "2h 13m",
-    poster: "interstellartest.jpg",
-    genres: ["action", "sci-fi"],
-    rating: 86,
-    tmdbId: "157336",
-  },
-  {
-    title: "Interstellar",
-    release_date: "2014",
-    certification: "PG-13",
-    runtime: "2h 13m",
-    poster: "interstellartest.jpg",
-    genres: ["action", "sci-fi"],
-    rating: 86,
-    tmdbId: "157336",
-  },
-];
-
 const Home: React.FC = () => {
+  const [movies, setMovies] = useState<any[]>([]);
+  const movieIds = [1, 2, 3];
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const moviePromises = movieIds.map((id) =>
+          fetch(`http://localhost:5001/getMovie?id=${id}`).then((res) => {
+            if (!res.ok) {
+              throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+          })
+        );
+        const moviesData = await Promise.all(moviePromises);
+        setMovies(moviesData);
+      } catch (error) {
+        console.error("Failed to fetch movies", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
     <div className="mt-32 mb-16 ml-8 space-y-8">
       <div>
         <span className="text-white text-2xl ml-5">Top Picks for You:</span>
-        <div className="flex overflow-x-scroll">
-          <MovieCarrousel movies={movies} />
-        </div>
-      </div>
-      <div>
-        <span className="text-white text-2xl ml-5">Comedy:</span>
-        <div className="flex overflow-x-scroll">
-          <MovieCarrousel movies={movies} />
-        </div>
-      </div>
-      <div>
-        <span className="text-white text-2xl ml-5">Horror:</span>
         <div className="flex overflow-x-scroll">
           <MovieCarrousel movies={movies} />
         </div>
--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import gzip
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
+CORS(app)  # This allows all origins by default
+
+# Load completed ratings matrix
+ratings_matrix = pd.read_csv("ratings_matrix.csv")
+ratings_matrix.set_index("userId", inplace=True)
 
 # Load movies data once when the server starts
 movies = {}
@@ -12,6 +19,35 @@ with gzip.open(filename, 'rb') as f:
     serialized_data = f.read()
     movies = pickle.loads(serialized_data)
 
+# Categorize movies by genre
+genres = ["Action", 
+          "Adventure", 
+          "Animation", 
+          "Children", 
+          "Comedy", 
+          "Crime", 
+          "Documentary", 
+          "Drama", 
+          "Fantasy", 
+          "Film-Noir", 
+          "Horror", 
+          "Musical", 
+          "Mystery", 
+          "Romance", 
+          "Sci-Fi", 
+          "Thriller", 
+          "War", 
+          "Western",
+          "IMAX"]
+
+movie_by_genre = {}
+for genre in genres:
+    movie_by_genre[genre] = []
+
+for movieId in movies.keys():
+    for genre in movies[movieId].genres:
+        movie_by_genre[genre].append(movies[movieId])
+
 @app.route("/getMovie")
 def getMovieByID():
     movie_id = request.args.get('id', type=int)
@@ -20,6 +56,16 @@ def getMovieByID():
         return jsonify(testMovie.to_dict())
     else:
         return jsonify({"error": "Movie not found"}), 404
+
+@app.route("/getMoviesToRate")
+def getMoviesToRate():
+    genres = request.args.get('genres', type=str)
+    genres_picked_by_user = genres.split("%2C")
+    movies_to_rate = [movie_by_genre[genre][:5] for genre in genres_picked_by_user]
+
+@app.route("/getMoviePredictions")
+def predictMovies():
+    pass
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=False, threaded=False)
