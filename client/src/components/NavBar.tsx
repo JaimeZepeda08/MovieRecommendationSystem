748b1ff459f69bb60768f2664484b85039dd3677
Jaime Zepeda Pina <zepedapina@wisc.edu>
2024-08-26 14:35:14 -0500

    About link now redirects to GitHub page

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 
 export default function NavBar() {
-  const links = [{ name: "About", href: "/about" }];
+  const links = [
+    {
+      name: "About",
+      href: "https://github.com/JaimeZepeda08/MovieRecommendationSystem",
+    },
+  ];
   const title = "Fakeflix";
   const titleArray = title.split("");
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <nav className="bg-zinc-950 bg-opacity-60 backdrop-blur-md text-white p-4 px-6 flex justify-between items-center shadow-2xl fixed top-0 w-full z-50">
       <a
         href="/"
-        className={`text-2xl font-bold py-3 px-4 text-yellow-600 ${
-          isHovered ? "" : "curved-title"
-        }`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        className="text-2xl font-bold py-3 px-4 text-yellow-600 curved-title"
       >
         {titleArray.map((letter, index) => (
           <span key={index} className={`letter-${index}`}>
